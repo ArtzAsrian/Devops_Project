6ba8dd88f4391afbabfe5ceeb4d4826b779e86de
Artur <arturasryan919@gmail.com>
2026-03-19 17:10:28 +0400

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello DevOps!")
+
+server = HTTPServer(("0.0.0.0", 8000), Handler)
+server.serve_forever()
